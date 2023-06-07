f7a0efd5486125b25bd8c6bef3ceeebc7100561d
Huse Kivrak <huse7@pm.me>
2023-06-07 13:45:17 -0400

    rough draft for api list views

--- project/app/authuser/views.py
+++ project/app/authuser/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.authentication import SessionAuthentication
+from serializers import LetterSerializer, EnvelopeSerializer
+from models import Letter, Envelope
+# from rest_framework.views import APIView
+# from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class LetterList(generics.ListCreateAPIView):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Letter.objects.all()
+    serializer_class = LetterSerializer
+
+
+class EnvelopeList(generics.ListCreateAPIView):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Envelope.objects.all()
+    serializer_class = EnvelopeSerializer
