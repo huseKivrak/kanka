fba096f3deb6c0a641820c8770b7c114ddf829c3
Huse Kivrak <huse7@pm.me>
2023-06-09 01:03:49 -0400

    acces/refresh, blacklist old refresh tokens

--- project/app/api/views.py
+++ project/app/api/views.py
@@ -1,1 +1,16 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    """Get list of all API routes"""
+
+    routes = [
+        '/api/token',
+        '/api/token/refresh',
+    ]
+
+
+    return Response(routes)
--- project/project/settings.py
+++ project/project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 # import os
 # import environ
 from pathlib import Path
+from datetime import timedelta
 
 # environ = environ.Env(
 #     DEBUG=(bool, True)
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
     'app',
     'psycopg2',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'debug_toolbar',
     'dotenv',
@@ -63,23 +65,12 @@ INSTALLED_APPS = [
 
 
 # CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOW_CREDENTIALS = True
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:5173',
-#     'http://127.0.0.1:',
-# ]
-
 
 REST_FRAMEWORK = {
 
-
-
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'authuser.jwt.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-
-
-
 
     # 'DEFAULT_AUTHENTICATION_CLASSES': [
     #     'rest_framework.authentication.TokenAuthentication',
@@ -88,9 +79,46 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated',
     # ]
-
 }
 
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=180), #stay logged in for 6 months
+    "ROTATE_REFRESH_TOKENS": True, #rolling refresh token
+    "BLACKLIST_AFTER_ROTATION": True, #blacklist old refresh tokens
+    "UPDATE_LAST_LOGIN": False,
+
+    "ALGORITHM": "HS256",
+    "VERIFYING_KEY": "",
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "JSON_ENCODER": None,
+    "JWK_URL": None,
+    "LEEWAY": 0,
+
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+
+    "JTI_CLAIM": "jti",
+
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+
+    "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
+    "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
+    "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
+    "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
+    "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
+    "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
