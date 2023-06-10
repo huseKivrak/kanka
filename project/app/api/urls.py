b7b52332c88ad957a63681af5d732c36dbdf08f5
Huse Kivrak <huse7@pm.me>
2023-06-10 18:00:14 -0400

    slash format

--- project/app/api/urls.py
+++ project/app/api/urls.py
@@ -5,7 +5,7 @@ from .views import MyTokenObtainPairView
 
 urlpatterns = [
     path('', views.getRoutes, name='routes'),
-    path('letters/', views.getLetters, name='letters'),
+    path('letters', views.getLetters, name='letters'),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
