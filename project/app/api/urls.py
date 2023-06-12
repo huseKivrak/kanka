d29a752cafd51dde24a0a6e122ed4cebf90b2fe6
Huse Kivrak <huse7@pm.me>
2023-06-11 21:58:47 -0400

    restful class views refactor

--- project/app/api/urls.py
+++ project/app/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import MyTokenObtainPairView
+from .views import MyTokenObtainPairView, LetterList, LetterDetail
 
 urlpatterns = [
     path('', views.getRoutes, name='routes'),
-    path('letters', views.getLetters, name='letters'),
+    path('letters/', LetterList.as_view(), name='letters'),
+    path('letters/<int:pk>/', LetterDetail.as_view(), name='letter-detail'),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- project/app/api/views.py
+++ project/app/api/views.py
@@ -1,8 +1,9 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import status
 
 from letter.models import Letter
 from letter.serializers import LetterSerializer
@@ -13,11 +14,8 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
     @classmethod
     def get_token(cls, user):
-
         token = super().get_token(user)
-
         token['username'] = user.username
-
         return token
 
 
@@ -25,22 +23,66 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
-@api_view(['GET'])
-def getRoutes(request):
-    """Get list of all API routes"""
+class LetterList(APIView):
+    permission_classes = [IsAuthenticated]
 
-    routes = [
-        '/api/token',
-        '/api/token/refresh',
-    ]
+    def get(self, request):
+        user = request.user
+        authored_letters = Letter.objects.filter(
+            author=user,
+            status=Letter.DRAFT
+        )
+        received_letters = Letter.objects.filter(
+            recipient=user,
+            status__in=[Letter.DELIVERED, Letter.READ]
+        )
+        letters = authored_letters.union(received_letters)
 
-    return Response(routes)
+        serializer = LetterSerializer(letters, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        user = request.user
+        serializer = LetterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save(author=user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def getLetters(request):
-    user = request.user
-    letters = Letter.objects.filter(current_owner=user)
-    serializer = LetterSerializer(letters, many=True)
-    return Response(serializer.data)
+class LetterDetail(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk, user):
+        try:
+            return Letter.objects.get(id=pk, author=user)
+        except Letter.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        letter = self.get_object(pk, request.user)
+        if letter is None:
+            return Response({'message': 'Letter does not exist or no view permission'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = LetterSerializer(letter)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        letter = self.get_object(pk, request.user)
+        if letter is None:
+            return Response({'message': 'Letter does not exist or no update permission'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = LetterSerializer(letter, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        letter = self.get_object(pk, request.user)
+        if letter is None:
+            return Response({'message': 'Letter does not exist or no delete permission'}, status=status.HTTP_404_NOT_FOUND)
+
+        letter.delete()
+        return Response({'message': 'Letter has been deleted'}, status=status.HTTP_200_OK)
