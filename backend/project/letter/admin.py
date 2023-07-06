5c23bee690d07ec07da43e924788dbdfd5efb6a0
Huse Kivrak <huse7@pm.me>
2023-07-05 22:14:34 -0400

    envelope removal cont

--- backend/project/letter/admin.py
+++ backend/project/letter/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import Letter, Envelope
+from .models import Letter
 
 admin.site.register(Letter)
-admin.site.register(Envelope)
--- backend/project/letter/serializers.py
+++ backend/project/letter/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Letter, Envelope
+from .models import Letter
 
 class LetterSerializer(serializers.ModelSerializer):
     class Meta:
