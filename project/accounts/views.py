a9573d9a6f60bc6e2879f522c38ac31120d9b669
Huse Kivrak <huse7@pm.me>
2023-07-16 15:19:38 -0400

    logout functionality

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib import messages
 from . forms import CustomUserCreationForm
 
-
-def login(request):
-    return render(request, "login.html")
-
-def profile(request):
-    return render(request, "profile.html")
 
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_user = form.save()
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'],
+                                    )
+            auth_login(request, new_user)
             return redirect('profile')
     else:
         form = CustomUserCreationForm()
     return render(request, "register.html", {'form': form})
+
+
+def login(request):
+    return render(request, "login.html")
+
+
+def profile(request):
+    return render(request, "profile.html")
+
+
+def logout(request):
+    auth_logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect('/')
