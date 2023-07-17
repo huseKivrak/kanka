2eed93485757b7a452ffec7203deee622384167f
Huse Kivrak <huse7@pm.me>
2023-07-17 17:45:44 -0400

    import after ready

--- project/letters/apps.py
+++ project/letters/apps.py
@@ -1,10 +1,11 @@
 from django.apps import AppConfig
-from utils import delivery_scheduler
+
 
 class LettersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'letters'
 
     def ready(self):
+        from letters.utils import delivery_scheduler
         print("starting delivery scheduler")
         delivery_scheduler.start()
