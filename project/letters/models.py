254786b38c8f95f225f590b9cadad59c16939994
Huse Kivrak <huse7@pm.me>
2023-07-17 17:41:28 -0400

    modified owner prop

--- project/letters/models.py
+++ project/letters/models.py
@@ -111,7 +111,10 @@ class Letter(TrackingModel):
 
     def save(self, *args, **kwargs):
         if not self.owner_id:
-            self.owner_id = self.author_id
+            if self.status == 'draft':
+                self.owner_id = self.author_id
+            else:
+                self.owner_id = self.recipient_id
         super().save(*args, **kwargs)
 
     def __str__(self):
