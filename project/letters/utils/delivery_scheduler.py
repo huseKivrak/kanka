8d478a87b32ef12c5dde5b2ac5240486969798ec
Huse Kivrak <huse7@pm.me>
2023-07-18 00:26:38 -0400

    logging added

--- project/letters/utils/delivery_scheduler.py
+++ project/letters/utils/delivery_scheduler.py
@@ -1,23 +1,25 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from letters.models import Letter
 from django.utils import timezone
+import logging
 
 scheduler = BackgroundScheduler()
-
+logger = logging.getLogger(__name__)
 
 def deliver_letters():
     letters = Letter.letters.sents()
     for letter in letters:
         if letter.delivery_date <= timezone.now():
             letter.deliver()
-            print(f"Delivered {letter.title} to {letter.recipient}")
+            logger.info(f"Delivered letter {letter.pk} to {letter.recipient}")
+
 
 
 def start():
     scheduler.add_job(
         deliver_letters,
         'interval',
-        hours=1,
+        minutes=1,
         id='deliver_letters',
         replace_existing=True,
     )
--- project/letters/utils/google_api.py
+++ project/letters/utils/google_api.py
@@ -3,18 +3,25 @@ import os
 import googlemaps
 from django.utils import timezone
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
+logger = logging.getLogger(__name__)
+
 
 # Google Maps API Key
 API_KEY = os.getenv("GOOGLE_DISTANCE_MATRIX_API_KEY")
 # Google Maps API Client
 gmaps = googlemaps.Client(key=API_KEY)
 
+
 class DeliveryEstimator:
     """
-    Utility class for estimating delivery date of a letter based on the
+    Utility class for determining delivery date of a letter based on the
     distance between author and recipient zip codes.
+        1. Google Distance Matrix API Call for distance
+        2. Approximate delivery days
+        3. Return as new datetime
     """
 
     def __init__(self, gmaps_client=gmaps):
@@ -34,13 +41,14 @@ class DeliveryEstimator:
             )
 
             if matrix["status"] != "OK":
-                print(
-                    f"Error with distance matrix request: {matrix['error_message']}")
+                logger.error(
+                    f"Matrix error: {matrix['status']}; {matrix['error_message']}")
                 return None
 
             element = matrix["rows"][0]["elements"][0]
             if element["status"] != "OK":
-                print(f"Error calculating distance: {element['status']}")
+                logger.error(
+                    f"Element error: {element['status']}; {element['error_message']}")
                 return None
 
             # Get distance
@@ -49,44 +57,62 @@ class DeliveryEstimator:
             return distance
 
         except Exception as e:
-            print(e)
+            logger.exception(e)
             return None
 
     def estimate_delivery_days(self, distance):
         """
-        Estimates delivery time based on distance (miles).
+        Just returns an integer between 1-5 based on distance value.
+
+        Separated out for easy updating/replacing later.
         """
-        if distance <= 50:
-            return 1
-        elif distance <= 300:
-            return 2
-        elif distance <= 1000:
-            return 3
-        elif distance <= 2000:
-            return 4
-        else:
-            return 5
+
+        delivery_days = [
+            (50, 1),
+            (300, 2),
+            (1000, 3),
+            (2000, 4),
+        ]
+
+        for max_distance, days in delivery_days:
+            if distance <= max_distance:
+                return days
+
+        return 5
 
     def calculate_delivery_date(self, send_date, delivery_days):
         """
-        Calculates delivery date based on send date and delivery days.
+        Returns a new datetime that is the send_date + 1-5 days.
+            - if that date falls on a Sunday, adds an extra day.
+
+        As above, separated out for easy updating/replacing later.
         """
-        return send_date + timezone.timedelta(days=delivery_days)
+        date = send_date + timezone.timedelta(days=delivery_days)
+
+        if date.weekday() == 6:
+            date += timezone.timedelta(days=1)
+            logger.debug(f"Sunday delivery; new date: {date}")
+
+        return date
 
     def get_delivery_date(self, zip1, zip2, send_date):
+        """
+        Facade method for getting delivery date. Also
+        """
         try:
             distance = self.get_distance_between_zip_codes(zip1, zip2)
-            delivery_days = self.estimate_delivery_days(distance)
-            delivery_date = self.calculate_delivery_date(send_date, delivery_days)
+            logger.debug(
+                f"Distance between {zip1} and {zip2} is {distance} miles")
 
-            # Add 1 day if delivery date falls on a Sunday
-            if delivery_date.weekday() == 6:
-                delivery_date += timezone.timedelta(days=1)
+            delivery_days = self.estimate_delivery_days(distance)
+            logger.debug(f"Delivery days: {delivery_days}")
+
+            delivery_date = self.calculate_delivery_date(
+                send_date, delivery_days)
+            logger.debug(f"Delivery date: {delivery_date}")
 
             return delivery_date
 
         except Exception as e:
-            print(f"failed to get delivery date: {e}")
+            logger.exception(e)
             return None
-
-
