fccd1b3635064a1f16c8aaa10732d0f49a620224
Huse Kivrak <huse7@pm.me>
2023-07-19 11:51:37 -0400

    customized login form

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -1,12 +1,10 @@
 # User / Auth related urls
-from django.contrib.auth import views as auth_views
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("register/", views.register, name="register"),
-    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("login/", views.login, name="login"),
     path("profile/", views.profile, name="profile"),
     path("logout/", views.logout, name="logout"),
 ]
