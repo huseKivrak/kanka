004c1f62a386f377ccdca46064a929254c7caa11
Huse Kivrak <huse7@pm.me>
2023-07-23 15:50:55 -0400

    removed 'detail' from path, more draft updates

--- project/letters/urls.py
+++ project/letters/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "letters"
 urlpatterns = [
     path("create/", views.create, name="create"),
-    path("detail/<int:pk>/", views.detail, name="detail"),
+    path("<int:pk>/", views.detail, name="detail"),
     path("mailbox/", views.mailbox, name="mailbox"),
     path("drafts/", views.drafts, name="drafts")
 ]
