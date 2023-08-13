62f70a763146c6f57f72fc9f7f14b87bdf5eabd6
Huse Kivrak <huse7@pm.me>
2023-08-13 15:34:34 -0400

    Friend_Request model, friends prop

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,7 +1,9 @@
 
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager, AbstractUser
 from django.core.validators import RegexValidator
+
+
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
@@ -17,13 +19,17 @@ class UserProfileManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, username, zip_code, password):
-        user = self.create_user(email, username, zip_code, password)
+        user = self.model(email=email, username=username,
+                          zip_code=zip_code)
+        user.set_password(password)
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
 
+        return user
 
-class CustomUser(AbstractUser, PermissionsMixin):
+
+class CustomUser(AbstractUser):
     email = models.EmailField(
         unique=True,
     )
@@ -46,6 +52,13 @@ class CustomUser(AbstractUser, PermissionsMixin):
         )]
     )
 
+    friends = models.ManyToManyField(
+        'self',
+        symmetrical=True,
+        through='FriendRequest',
+        through_fields=('from_user', 'to_user'),
+    )
+
     objects = UserProfileManager()
 
     REQUIRED_FIELDS = ['email', 'zip_code']
@@ -53,3 +66,49 @@ class CustomUser(AbstractUser, PermissionsMixin):
     def __str__(self):
         return self.username
 
+
+class FriendRequest(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('rejected', 'Rejected'),
+    ]
+
+    from_user = models.ForeignKey(
+        CustomUser, related_name='from_user', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(
+        CustomUser, related_name='to_user', on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=8, choices=STATUS_CHOICES, default='pending')
+
+    def __str__(self):
+        return f'{self.from_user} - {self.to_user}'
+
+    class Meta:
+        unique_together = ('from_user', 'to_user')
+        index_together = ('from_user', 'to_user')
+
+    def accept(self):
+        """Accept friend request"""
+        self.status = 'accepted'
+        self.remove_request()
+
+        self.from_user.friends.add(self.to_user)
+        self.to_user.friends.add(self.from_user)
+
+        self.delete()
+
+    def reject(self):
+        """Reject friend request"""
+        self.status = 'rejected'
+        self.save()
+        self.remove_request()
+
+    def remove_request(self):
+        """Remove friend request"""
+        super(FriendRequest, self).delete()
+
+    @classmethod
+    def get_requests(cls, user):
+        """Return all pending friend requests for a user"""
+        return cls.objects.filter(to_user=user, status='pending')
